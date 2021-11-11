090d88dad0c8046f2e7218d9d219e8b037682fbe
Igor Jankowski <jankowskiigor5@gmail.comgit congig --global -lgit config --global user.email jankowskiigor5@gmail.com>
2021-11-11 14:37:04 +0100

    added styles for all resolutions

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,26 @@
-const button = document.querySelector('.menu');
-const sidebar = document.querySelector('.mobile-nav');
-const overlay = document.querySelector('.overlay');
+// const button = document.querySelector('.menu');
+// const sidebar = document.querySelector('.mobile-nav');
+// const overlay = document.querySelector('.overlay');
 
-button.addEventListener('click', function() {
-  sidebar.classList.toggle('is-active');
-  overlay.classList.toggle('is-active');
-});
+// button.addEventListener('click', function() {
+//   sidebar.classList.toggle('is-active');
+//   overlay.classList.toggle('is-active');
+// });
 
-overlay.addEventListener('click', function() {
-  sidebar.classList.remove('is-active');
-  overlay.classList.remove('is-active');
-});
+// overlay.addEventListener('click', function() {
+//   sidebar.classList.remove('is-active');
+//   overlay.classList.remove('is-active');
+// });
 
+const header = document.querySelector("header");
+const body = document.querySelector("body");
+const options = {};
+const observer = new IntersectionObserver(function (entries, observer) {
+  entries.forEach(entry => {
+    if(entry){
+      header.classList.add('header-scrolled');
+    }
+  })
+}, options);
+
+observer.observe(body)
