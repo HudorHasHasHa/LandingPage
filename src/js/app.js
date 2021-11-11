dd56d540ddd210adf0484567d543bee50bfa58a4
Igor Jankowski <jankowskiigor5@gmail.comgit congig --global -lgit config --global user.email jankowskiigor5@gmail.com>
2021-11-11 17:33:10 +0100

    cleared comments

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,3 @@
-// const button = document.querySelector('.menu');
-// const sidebar = document.querySelector('.mobile-nav');
-// const overlay = document.querySelector('.overlay');
-
-// button.addEventListener('click', function() {
-//   sidebar.classList.toggle('is-active');
-//   overlay.classList.toggle('is-active');
-// });
-
-// overlay.addEventListener('click', function() {
-//   sidebar.classList.remove('is-active');
-//   overlay.classList.remove('is-active');
-// });
-
 let position = window.scrollY;
 const header = document.querySelector("header");
 const header_height = header.offsetHeight;
